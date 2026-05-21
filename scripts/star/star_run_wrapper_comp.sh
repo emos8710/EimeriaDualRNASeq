2908fbd1ec73b647ef2b6d9d005fd1837a557fcf
Emma Östlund <emma.ostlund@sva.se>
2026-05-21 13:12:38 +0200

    edit star map script

--- scripts/star/star_run_wrapper_comp.sh
+++ scripts/star/star_run_wrapper_comp.sh
@@ -3,9 +3,9 @@
 #SBATCH -n 8
 #SBATCH --mem=20000M
 #SBATCH -t 48:00:00
-#SBATCH -J star_run_merged
+#SBATCH -J star_run_chicken
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user emma.ostlund@sva.se
 
 # Your commands
-bash EimeriaDualRNASeq/scripts/star/star_run.sh results/fastp/IBV/
+bash EimeriaDualRNASeq/scripts/star/star_run.sh results/fastp/Ascaridia/
