c57941dc78f63a4f8cf175173ebe5be1d66c8177
Emma Östlund <emma.ostlund@sva.se>
2026-05-21 11:41:18 +0200

    edit star index script

--- scripts/star/star_index.sh
+++ scripts/star/star_index.sh
@@ -1,7 +1,7 @@
 # Script for producing an index for STAR
 # Usage:  bash star_index.sh /Path/to/reference/genome.fna /Path/to/reference/genome.gff
 
-mkdir -p results/star/index
+mkdir -p results/star/index_chicken
 
 software/STAR/source/STAR --runMode genomeGenerate --genomeDir results/star/index --genomeFastaFiles ${1} --sjdbGTFtagExonParentTranscript Parent \
 	--sjdbGTFtagExonParentGene gene --runThreadN 8 --sjdbGTFfile ${2} --sjdbOverhang 150 --genomeChrBinNbits 18 \
--- scripts/star/star_index_wrapper.sh
+++ scripts/star/star_index_wrapper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
 #SBATCH -n 8
-#SBATCH --mem=20000M
+#SBATCH --mem=40000M
 #SBATCH -t 1:00:00
-#SBATCH -J star_index_merged
+#SBATCH -J star_index_chicken
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user emma.ostlund@sva.se
 
 # Your commands
-bash EimeriaDualRNASeq/scripts/star/star_index.sh reference_genomes/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b_IBV-M41.fna reference_genomes/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b_IBV-M41.gff
+bash EimeriaDualRNASeq/scripts/star/star_index.sh reference_genomes/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b.fna reference_genomes/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b.gff
