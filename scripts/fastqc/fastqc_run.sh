086d25a5acbc786869adcea1d00f09d7980c674d
Emma Östlund <emma.ostlund@sva.se>
2026-05-21 10:18:56 +0200

    edit fastqc script

--- scripts/fastqc/fastqc_run.sh
+++ scripts/fastqc/fastqc_run.sh
@@ -1,9 +1,5 @@
 # Script for running FastQC on a set of Trimmomatic trimmed read files and outputting both FastQC and MultiQC reports for them
 # Usage: bash fastqc_run.sh /Path/to/folder/containing/trimmed/read/files1 /Path/to/folder/containing/trimmed/read/files2 ...
 
-for FILE_PATH in "$@"
-do
-    GROUP_NAME=$(echo ${FILE_PATH} | cut -d '/' -f 3)
-    mkdir -p results/fastqc/${GROUP_NAME} 
-    software/FastQC/fastqc -t 2 -o results/fastqc/${GROUP_NAME} ${FILE_PATH}/*.fastq.gz
-done
+
+software/FastQC/fastqc -t 18 -o results/fastqc/ "$@"
