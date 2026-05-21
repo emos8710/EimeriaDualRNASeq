9cba8b940503c1a8a477cb91c6186125a7577a4c
Emma Östlund <emma.ostlund@sva.se>
2026-05-21 12:19:44 +0200

    edit star map script

--- scripts/star/star_run.sh
+++ scripts/star/star_run.sh
@@ -4,14 +4,14 @@
 GROUP_NAME=$(echo ${1} | cut -d '/' -f 3)
 mkdir -p results/star/mapped_reads/${GROUP_NAME}
 
-for f in $(ls ${1}/*_paired.fastq.gz | sed 's/_R._paired.fastq.gz//' | sort -u)
+for f in $(ls ${1}/*/*_paired.fastq.gz | sed 's/_R._paired.fastq.gz//' | sort -u)
 do
     OUTPUT_NAME=$(echo ${f} | rev | cut -d '/' -f 1 | rev)
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${f}_R1_paired.fastq.gz ${f}_R2_paired.fastq.gz --readFilesCommand gunzip -c \
+    STAR --genomeDir /proj/naiss2025-23-525/results/star/index --readFilesIn ${f}_R1_paired.fastq.gz ${f}_R2_paired.fastq.gz --readFilesCommand gunzip -c \
 		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${OUTPUT_NAME}_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${f}_R1_unpaired.fastq.gz --readFilesCommand gunzip -c \
+    STAR --genomeDir /proj/naiss2025-23-525/results/star/index --readFilesIn ${f}_R1_unpaired.fastq.gz --readFilesCommand gunzip -c \
 		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${OUTPUT_NAME}_R1_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${f}_R2_unpaired.fastq.gz --readFilesCommand gunzip -c \
+    STAR --genomeDir /proj/naiss2025-23-525/results/star/index --readFilesIn ${f}_R2_unpaired.fastq.gz --readFilesCommand gunzip -c \
 		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${OUTPUT_NAME}_R2_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
 done
 
--- scripts/star/star_run_wrapper_comp.sh
+++ scripts/star/star_run_wrapper_comp.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH -A snic2020-15-16
-#SBATCH -p core
 #SBATCH -n 8
 #SBATCH -t 48:00:00
-#SBATCH -J star_run_comp
+#SBATCH -J star_run_merged
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
-
-module load bioinfo-tools
-module load star/2.7.2b
+#SBATCH --mail-user emma.ostlund@sva.se
 
 # Your commands
-bash scripts/star/star_run.sh results/trimmomatic/in_vitro_complementary
+bash EimeriaDualRNASeq/scripts/star/star_run.sh results/fastp/IBV/
