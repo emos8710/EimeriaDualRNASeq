9b0ccd5c0fa542ce487c9f3cfdcb4b13e5542e98
Emma Östlund <emma.ostlund@sva.se>
2026-05-22 14:53:08 +0200

    edit htseq-count script

--- scripts/htseq/htseq_wrapper_rev_comp.sh
+++ scripts/htseq/htseq_wrapper_rev_comp.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH -A snic2020-15-16
-#SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 48:00:00
-#SBATCH -J htseq_run_rev_comp
+#SBATCH -J htseq_run_IBV
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
-
-module load bioinfo-tools
-module load htseq/0.9.1
+#SBATCH --mail-user emma.ostlund@sva.se
 
 # Your commands
-bash scripts/htseq/htseq_rev_run.sh results/star/mapped_reads/in_vitro_complementary
+bash scripts/htseq/htseq_rev_run.sh results/star/mapped_reads/IBV
