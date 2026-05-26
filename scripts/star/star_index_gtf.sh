ef9d78fa7e3fbce1bcbe14b7fac3596a07e7d9c0
Emma Östlund <emma.ostlund@sva.se>
2026-05-26 14:40:36 +0200

    edit star index script

--- scripts/star/star_index_gtf.sh
+++ scripts/star/star_index_gtf.sh
@@ -1,9 +1,9 @@
 # Script for producing an index for STAR
 # Usage:  bash star_index.sh /Path/to/reference/genome.fna /Path/to/reference/genome.gff
 
-mkdir -p results/star/index_chicken_IBV
+mkdir -p results/star/index_chicken
 
-software/STAR/source/STAR --runMode genomeGenerate --genomeDir results/star/index --genomeFastaFiles ${1} \
+software/STAR/source/STAR --runMode genomeGenerate --genomeDir results/star/index_chicken --genomeFastaFiles ${1} \
 	--runThreadN 8 --sjdbGTFfile ${2} --sjdbOverhang 150 --genomeChrBinNbits 18 \
 	--genomeSAindexNbases 13
 
