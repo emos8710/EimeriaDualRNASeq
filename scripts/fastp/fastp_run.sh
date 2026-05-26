f03f0c3129af6f7c9179120ea4e82530c99d84fb
Emma Östlund <emma.ostlund@sva.se>
2026-05-26 14:30:05 +0200

    edit star index script

--- scripts/fastp/fastp_run.sh
+++ scripts/fastp/fastp_run.sh
@@ -3,19 +3,19 @@
 
 for FILE_PATH in "$@"
 do
-    GROUP_NAME=$(echo ${FILE_PATH} | cut -d '/' -f 2)
+    GROUP_NAME=$(echo ${FILE_PATH} | cut -d '/' -f 4)
     mkdir -p results/fastp/${GROUP_NAME}
     for f in $(ls ${FILE_PATH} | sed 's/_.._001.fastq.gz//' | sort -u)
     do
-        software/fastp --thread 15 \
+        fastp --thread 10 \
         -i ${FILE_PATH}/${f}_R1_001.fastq.gz \
         -I ${FILE_PATH}/${f}_R2_001.fastq.gz \
         -o results/fastp/${GROUP_NAME}/${f}_R1_paired.fastq.gz \
         --unpaired1 results/fastp/${GROUP_NAME}/${f}_R1_unpaired.fastq.gz \
         -O results/fastp/${GROUP_NAME}/${f}_R2_paired.fastq.gz \
-        -h results/fastp/${GROUP_NAME}/${f}_report.html \
-        -j results/fastp/${GROUP_NAME}/${f}_report.json \
         --unpaired2 results/fastp/${GROUP_NAME}/${f}_R2_unpaired.fastq.gz \
+        --html results/fastp/${GROUP_NAME}/${f}_report.html \
+        --json results/fastp/${GROUP_NAME}/${f}_report.json \
         --detect_adapter_for_pe \
         --cut_front -M 20 -W 4 \
         --trim_poly_g \
---
+++ scripts/star/star_index_gtf.sh
@@ -0,0 +1,10 @@
+# Script for producing an index for STAR
+# Usage:  bash star_index.sh /Path/to/reference/genome.fna /Path/to/reference/genome.gff
+
+mkdir -p results/star/index_chicken_IBV
+
+software/STAR/source/STAR --runMode genomeGenerate --genomeDir results/star/index --genomeFastaFiles ${1} \
+	--runThreadN 8 --sjdbGTFfile ${2} --sjdbOverhang 150 --genomeChrBinNbits 18 \
+	--genomeSAindexNbases 13
+
+	#--genomeSAindexNbases 13 set on recommentation from STAR
