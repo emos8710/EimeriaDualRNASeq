9d5b4c10ac44cafefb98e4efa80fce2d97d2325b
Emma Östlund <emma.ostlund@sva.se>
2026-05-26 15:06:05 +0200

    edit star index script

--- scripts/star/star_run_wrapper_comp.sh
+++ scripts/star/star_run_wrapper_comp.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 8
 #SBATCH --mem=20000M
 #SBATCH -t 48:00:00
-#SBATCH -J star_run_chicken
+#SBATCH -J star_run_Ascaridia
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user emma.ostlund@sva.se
 
