1c0b7d16804cfbc416fbb0e473b8a610e9cc525b
Emma Östlund <emma.ostlund@sva.se>
2026-05-27 08:45:10 +0200

    edit htseq-count script

--- scripts/htseq/htseq_wrapper_rev_comp.sh
+++ scripts/htseq/htseq_wrapper_rev_comp.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -n 10
 #SBATCH -t 48:00:00
-#SBATCH -J htseq_run_IBV
+#SBATCH -J htseq_run_Ascaridia
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user emma.ostlund@sva.se
 
 # Your commands
-bash EimeriaDualRNASeq/scripts/htseq/htseq_rev_run_gtf.sh results/star/mapped_reads/IBV
+bash EimeriaDualRNASeq/scripts/htseq/htseq_rev_run_gtf.sh results/star/mapped_reads/Ascaridia
